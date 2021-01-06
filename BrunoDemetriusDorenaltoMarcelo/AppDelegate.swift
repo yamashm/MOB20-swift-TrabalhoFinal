fe1a710adc594fbcfbcc6cb05d6d6e9d671f0bbc
Marcelo Yamashita <awnmist@gmail.com>
2021-01-06 18:05:19 -0500

    Primeiro commit

---
+++ BrunoDemetriusDorenaltoMarcelo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BrunoDemetriusDorenaltoMarcelo
+//
+//  Created by user189149 on 1/6/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+   
+
+}
+
---
+++ BrunoDemetriusDorenaltoMarcelo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrunoDemetriusDorenaltoMarcelo
+//
+//  Created by user189149 on 1/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
